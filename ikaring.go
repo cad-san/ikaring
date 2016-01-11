bbdec156e1ea06cc5214626febdcc0533be57b4a
cad-san <cadsan1986@gmail.com>
2016-01-11 15:35:23 +0900

    implement ikaClient.getStageInfo()

--- ikaring.go
+++ ikaring.go
@@ -1,22 +1,69 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
 
 	"github.com/PuerkitoBio/goquery"
+	simplejson "github.com/bitly/go-simplejson"
 )
 
 type ikaClient http.Client
+
+type stage struct {
+	Name  string `json:"name"`
+	Image string `json:"asset_path"`
+}
+
+type regulation struct {
+	Regular []stage
+	Gachi   []stage
+}
+
+type schedule struct {
+	TimeBegin string     `json:"datetime_begin"`
+	TimeEnd   string     `json:"datetime_end"`
+	Stages    regulation `json:"stages"`
+	GachiRule string     `json:"gachi_rule"`
+}
+
+type stageInfo struct {
+	Festival  bool       `json:"festival"`
+	Schedules []schedule `json:"schedule"`
+}
 
 const (
 	splatoonClientID = "12af3d0a3a1f441eb900411bb50a835a"
 	splatoonOauthURL = "https://splatoon.nintendo.net/users/auth/nintendo"
 	nintendoOauthURL = "https://id.nintendo.net/oauth/authorize"
+
+	splatoonScheduleAPI = "https://splatoon.nintendo.net/schedule/index.json"
 )
+
+func checkJSONError(data []byte) error {
+	js, err := simplejson.NewJson(data)
+	if err != nil {
+		return err
+	}
+
+	if info := js.Get("error").MustString(); len(info) != 0 {
+		return errors.New(info)
+	}
+	return nil
+}
+
+func decodeJSONSchedule(data []byte) (*stageInfo, error) {
+	info := &stageInfo{}
+	if err := json.Unmarshal(data, info); err != nil {
+		return nil, err
+	}
+	return info, nil
+}
 
 func getOauthQuery(oarthURL string, id string, password string) (url.Values, error) {
 	doc, err := goquery.NewDocument(oarthURL)
@@ -95,5 +142,21 @@ func (c *ikaClient) login(name string, password string) error {
 	return nil
 }
 
+func (c *ikaClient) getStageInfo() (*stageInfo, error) {
+
+	resp, err := (*http.Client)(c).Get(splatoonScheduleAPI)
+
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return decodeJSONSchedule(resp.Body)
+}
+
 func main() {
+	_, err := createClient()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
